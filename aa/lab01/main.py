a88d8eda813d326104133587191e53607bd9dad8
nikita <nikita.garasev@mail.ru>
2020-09-06 14:22:49 +0300

    test.py was created

--- aa/lab01/main.py
+++ aa/lab01/main.py
@@ -1,22 +1,11 @@
-
 import numpy as np
 import string
 import random
 
-from time import time
+from time import process_time
 
 symbols = string.ascii_letters + string.digits
 debug = False
-
-
-# Вспомогательные функции
-def min_three(a, b, c):
-    if a <= b and a <= c:
-        return a
-    elif b <= c and b <= a:
-        return b
-    elif c <= a and c <= b:
-        return c
 
 
 def random_string(length):
@@ -24,13 +13,13 @@ def random_string(length):
 
 
 def time_analyze(function, iteration, length=5):
-    t1 = time()
+    t1 = process_time()
     for i in range(iteration):
         s1 = random_string(length)
         s2 = random_string(length)
-        function(s1, s2)
-    t2 = time()
-    return (t2 - t1) / iter
+        function(s1, s2, False)
+    t2 = process_time()
+    return (t2 - t1) / iteration
 
 
 def input_s1_s2():
@@ -125,16 +114,17 @@ def calc_dist_recur(s1, s2, printable=False):
 
 
 def calc_dist_recur_matrix(s1, s2, printable=False):
-    matr = np.full((len(s1) + 1, len(s2) + 1), -1)
-
-    def calc_value(i, j):
+    def calc_value(matr, i, j):
         if matr[i][j] != -1:
             return matr[i][j]
         else:
             tmp = 0 if (s1[i - 1] == s2[j - 1]) else 1
-            return min(calc_value(i - 1, j) + 1,
-                       calc_value(i, j - 1) + 1,
-                       calc_value(i - 1, j - 1) + tmp,)
+            matr[i][j] = min(calc_value(matr, i - 1, j) + 1,
+                             calc_value(matr, i, j - 1) + 1,
+                             calc_value(matr, i - 1, j - 1) + tmp)
+            return matr[i][j]
+
+    matr = np.full((len(s1) + 1, len(s2) + 1), -1)
 
     for i in range(len(s1) + 1):
         matr[i][0] = i
@@ -142,15 +132,20 @@ def calc_dist_recur_matrix(s1, s2, printable=False):
         matr[0][j] = j
 
     if printable:
-        print('Return:', calc_value(len(s1), len(s2)))
+        print('Return:', calc_value(matr, len(s1), len(s2)))
+        print('Matrix \n', matr)
+        print('Operation: \n   ', end='')
+        operations(s1, s2, matr)
 
 
 def calc_dist_damerau(s1, s2, printable=False):
     matr = np.eye(len(s1) + 1, len(s2) + 1)
+
     for i in range(len(s1) + 1):
         matr[i][0] = i
     for j in range(len(s2) + 1):
         matr[0][j] = j
+
     for i in range(len(s1)):
         for j in range(len(s2)):
             d1 = matr[i + 1][j] + 1
@@ -169,8 +164,9 @@ def calc_dist_damerau(s1, s2, printable=False):
         print('Matrix: \n', matr)
 
 
-if __name__ == '__main__':
+calc_func = [calc_dist_matrix, calc_dist_recur, calc_dist_recur_matrix, calc_dist_damerau]
 
+if __name__ == '__main__':
     while True:
         case = input('\n\nMenu: \n \
 1) Levenshtein distance matrix\n \
@@ -178,6 +174,7 @@ if __name__ == '__main__':
 3) Levenshtein distance matrix-recursion\n \
 4) Damerau Levenshtein distance matrix\n \
 5) Time analyze\n \
+6) Time analyze 1 of methods\n \
 case: ')
         if case == '1':
             start_func(calc_dist_matrix)
@@ -189,17 +186,23 @@ case: ')
             start_func(calc_dist_damerau)
         elif case == '5':
 
-            iter = int_inputer('Input number of iterations:', 100)
+            iteration = int_inputer('Input number of iterations:', 100)
             i1 = int_inputer('Input lower limit:', 1)
             i2 = int_inputer('Input upper limit:', 10)
 
             for i in range(i1, i2 + 1):
                 print("String length: ", i)
                 print("   Levenshtein matrix           : ", "{0:.8f}".format(time_analyze(calc_dist_matrix,
-                                                                                           iter, i)))
+                                                                                          iteration, i)))
                 print("   Levenshtein recursion        : ", "{0:.8f}".format(time_analyze(calc_dist_recur,
-                                                                                           iter, i)))
+                                                                                          iteration, i)))
                 print("   Levenshtein matrix-recursion : ", "{0:.8f}".format(time_analyze(calc_dist_recur_matrix,
-                                                                                           iter, i)))
+                                                                                          iteration, i)))
                 print("   Damerau Levenshtein matrix   : ", "{0:.8f}".format(time_analyze(calc_dist_damerau,
-                                                                                           iter, i)))
+                                                                                          iteration, i)))
+
+        elif case == '6':
+            func = int_inputer('  input number of method:', 1)
+            iteration = int_inputer('  input number of iterations:', 100)
+            i = int_inputer('  input length of word:', 1)
+            print("🕐 Time: ", "{0:.8f}".format(time_analyze(calc_func[func], iteration, i)))
